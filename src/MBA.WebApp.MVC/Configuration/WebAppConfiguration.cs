a2dbcf4b00a0a907b8ed94a4664945eddc31e8d1
Douglas costa <douglasalgustocosta@gmail.com>
2026-01-31 15:24:30 -0300

    Alteração e finalização do identity

---
+++ src/MBA.WebApp.MVC/Configuration/WebAppConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace MBA.WebApp.MVC.Controllers
+{
+    public static class WebAppConfiguration
+    {
+        public static void AddMvcConfiguration(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+        public static void UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        { }
+    }
+}
